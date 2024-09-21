5feddf41140d45c5e14f9d2e9f9ad15eed74cb36
ronreiter <ron.reiter@gmail.com>
2024-09-21 12:09:16 +0300

    huge

--- hugeai.py
+++ hugeai.py
@@ -1,9 +1,11 @@
 import os
 import argparse
+import random
 
 from PIL.Image import Image
+from PIL.PngImagePlugin import PngInfo
+
 from aura_sr import AuraSR
-from slugify import slugify
 
 import torch
 import diffusers
@@ -11,6 +13,7 @@ from diffusers import FluxPipeline
 from huggingface_hub import login
 import dotenv
 import RealESRGAN
+import hashlib
 
 
 torch.set_default_device("mps")
@@ -52,12 +55,20 @@ def generate_images(prompt, width, height, guidance, iterations, seed, num_image
     # pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", revision='refs/pr/1', torch_dtype=torch.bfloat16).to("mps")
     pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16).to("mps")
 
-    slug = slugify(prompt)
-    os.makedirs(f"outputs/{slug}", exist_ok=True)
+    dir_name = hashlib.md5(prompt.encode()).hexdigest()
+    os.makedirs(f"outputs/{dir_name}", exist_ok=True)
 
     for i in range(num_images):
         image_num = seed+i+1
-        filename = f"outputs/{slug}/it_{iterations}_guidance_{guidance}_sr_1_num_{image_num}.png"
+        base_filename = f"outputs/{dir_name}/num_{image_num}_it_{iterations}_guidance_{guidance}"
+        filename = f"{base_filename}_sr_1.png"
+
+        metadata = PngInfo()
+        metadata.add_text("Prompt", prompt)
+        metadata.add_text("Guidance", str(guidance))
+        metadata.add_text("Iterations", str(iterations))
+        metadata.add_text("Seed", str(seed))
+        metadata.add_text("Created With", "Huge AI")
 
         if os.path.exists(filename):
             out = Image.open(filename)
@@ -73,7 +84,7 @@ def generate_images(prompt, width, height, guidance, iterations, seed, num_image
                 generator=torch.Generator("mps").manual_seed(image_num)
             ).images[0]
 
-            out.save(filename)
+            out.save(filename, pnginfo=metadata)
             print(f"Saved image: {filename}")
 
         # Apply super resolution if requested
@@ -82,14 +93,16 @@ def generate_images(prompt, width, height, guidance, iterations, seed, num_image
             out = apply_super_resolution(out, sr_scale)
 
             # Save upscaled image
-            filename = f"outputs/{slug}/it_{iterations}_guidance_{guidance}_{'sr_' + str(sr_scale) if sr_scale else 'original'}_num_{image_num}.png"
-            out.save(filename)
+            filename = f"{base_filename}_{'sr_' + str(sr_scale) if sr_scale else 'original'}.png"
+            out.save(filename, pnginfo=metadata)
             print(f"Saved image: {filename}")
         elif sr_aura:
             print(f"Upscaling with aura...")
             out = apply_super_resolution_aura(out)
-            filename = f"outputs/{slug}/it_{iterations}_guidance_{guidance}_{'sr_aura'}_num_{image_num}.png"
-            out.save(filename)
+
+            # Save upscaled image
+            filename = f"{base_filename}_sr_aura.png"
+            out.save(filename, pnginfo=metadata)
 
 
 def main():
@@ -97,9 +110,9 @@ def main():
     parser.add_argument("prompt", type=str, help="Prompt for image generation")
     parser.add_argument("--width", type=int, default=1536, help="Image width (default: 1536)")
     parser.add_argument("--height", type=int, default=1024, help="Image height (default: 1024)")
-    parser.add_argument("--iterations", type=int, default=30, help="Iterations (default: 30)")
+    parser.add_argument("--iterations", type=int, default=50, help="Iterations (default: 50)")
     parser.add_argument("--guidance", type=float, default=3.5, help="Guidance (default: 3.5)")
-    parser.add_argument("--seed", type=int, default=0, help="Seed start (default: 0)")
+    parser.add_argument("--seed", type=int, default=0, help="Seed start (default: random)")
     parser.add_argument("--num", type=int, default=1, help="Number of images to generate (default: 1)")
     parser.add_argument("--sr", type=int, choices=[1, 2, 4, 8], default=8, help="Super resolution scale (1, 2, 4, or 8) (default: 8)")
     parser.add_argument("--sr-aura", action="store_true", default=False, help="Super resolution with Aura")
@@ -112,7 +125,7 @@ def main():
         height=args.height,
         guidance=args.guidance,
         iterations=args.iterations,
-        seed=args.seed,
+        seed=args.seed if args.seed else random.randint(0, 2 ** 32 - 1),
         num_images=args.num,
         sr_scale=args.sr,
         sr_aura=args.sr_aura,
